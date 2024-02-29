d9ccd17e2088dbcd98840bbef6fbdd6a207641a0
amiiiiee <amiameen507@gmail.com>
2024-02-29 09:46:27 +0530

    add-create-post

--- src/app.js
+++ src/app.js
@@ -1,12 +1,14 @@
 const express=require('express');
 const userRouter=require('./apis/router/userRouter')
 const postRouter = require('./apis/router/postRouter');
+const morgan = require('morgan');
 const app=express();
 const core=require('cors');
 
 
 app.use(core())
 app.use(express.json());
+app.use(morgan('dev'));
 app.use('/api/users/',userRouter)
 app.use('/api/users/',postRouter)
 
